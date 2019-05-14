216221d2dcdff0ba1995f4906b49c68808066fb8
Jason Calixto <JasonCalixto97@gmail.com>
2019-05-14 14:13:19 -0400

    last commit in YearUp, line fixes

--- main.js
+++ main.js
@@ -4,12 +4,18 @@ var drop
 
 document.getElementById("paps").addEventListener('click', cometh);
 
-
 function cometh() {
   for(drop of pMenu){
     drop.classList.toggle('pop');
     drop.classList.toggle('hidden');
-}
+  }
 }
 
-window.onclick = 
+window.onclick = function(event) {
+  if (!event.target.matches('.paper')) {
+    for(drop of pMenu){
+      drop.classList.remove('pop');
+      drop.classList.add('hidden');
+    }
+  }
+}
